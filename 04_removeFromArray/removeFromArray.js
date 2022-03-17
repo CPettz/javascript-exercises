03f756f3781812df19da264de2ddc7d0cacbeed5
Christian Pettigrew <christian.pettigrew123@gmail.com>
2022-03-16 21:32:06 -0400

    Complete excercise 4 and 5 of TOP JS Fundamentals
    Completed excercises 4 and 5 related to TOP Fundamentals Course.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+
+    for(let i = 0; i < toRemove.length; i++) {
+        if (arr.indexOf(toRemove[i]) === -1) {
+            continue;
+        } else {
+            arr.splice(arr.indexOf(toRemove[i]), 1);
+        }
+    }
+
+    return arr;
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (typeof num1 !== typeof num2) {
+        return 'ERROR';
+    } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return 'ERROR';
+    } else {
+        // We're going to be using the Gauss consecutive numbers approach for this
+        let range = Math.abs((num2 - num1)) + 1;
+        return result = (range / 2) * (num1 + num2);
+    }
 };
 
 // Do not edit below this line
