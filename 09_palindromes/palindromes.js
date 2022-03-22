79b770f2945fe3d06263b75386a9b51f7d8bac57
Christian Pettigrew <christian.pettigrew123@gmail.com>
2022-03-21 22:14:21 -0400

    Complete exercises 7 through 10 of TOP JS Lessons

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let original = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+    let noPunctuationLower = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+    let comparison = noPunctuationLower.split("").reverse().join("");
+    
+    if (original == comparison) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    
+    let fibNum = parseInt(fib);
 
+    if (fibNum < 0) {
+        return 'OOPS';
+    } else {
+        
+        let arr = [];
+        arr[0] = 1;
+        arr[1] = 1;
+    
+        for (let i = 2; i < fibNum; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+    
+        return arr[fibNum - 1];
+    }
+    
 };
 
 // Do not edit below this line
