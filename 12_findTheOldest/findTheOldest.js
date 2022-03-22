edca691f0593bece9171e6c0b82140e4d59dcdf7
Christian Pettigrew <christian.pettigrew123@gmail.com>
2022-03-22 19:40:25 -0400

    Complete excercises 12 and 13 of TOP JS Fund

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleObject) {
+
+    // Add age property to all peopleObjects
+    peopleObject.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth);
+
+    // Extra steps required in case the person is still living
+    let date = new Date().getFullYear();
+    peopleObject.forEach(function(person) {
+        if (isNaN(person.age) == true) {
+            person.age = date - person.yearOfBirth;
+        }
+    });
+
+    // Return the person object with the largest person.age property
+    let result = peopleObject.reduce(function(prev, curr) {
+        return prev.age < curr.age ? curr : prev;
+    });
+
+    return result;
 
 };
 
